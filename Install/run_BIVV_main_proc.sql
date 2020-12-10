880e08c0843798949b332735afbc8b397822c4be
Gedzior, Mario US EXT <Mario.Gedzior@sanofi.com>
2020-12-10 18:46:48 +0000

    git-tfs-id: [http://xspw11c700t:8080/tfs/Commercial/]$/Bioverativ/GPCS_HCRS/Oracle;C24300

---
+++ Install/run_BIVV_main_proc.sql
@@ -0,0 +1,6 @@
+BEGIN
+  -- Call the procedure
+   bivv.PKG_STG_MEDI.p_main ('Y', 'Y');
+   COMMIT;
+END;
+/
---
+++ Install/run_HCRS_main_proc.sql
@@ -0,0 +1,6 @@
+BEGIN
+  -- Call the procedure
+   hcrs.PKG_LOAD_BIVV_MEDI_DATA.p_main ('N');
+   COMMIT;
+END;
+/
