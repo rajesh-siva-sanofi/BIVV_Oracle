bcefe89931daa7639e5428f2826e93178995b36d
Gedzior, Mario US EXT <Mario.Gedzior@sanofi.com>
2021-01-25 18:41:58 +0000

    adding NYMCAPSUPP contract to exclusion list
    git-tfs-id: [http://xspw11c700t:8080/tfs/Commercial/]$/Bioverativ/GPCS_HCRS/Oracle;C24562

--- Views/BIVV/BIVV_CONT_EXCL_V.sql
+++ Views/BIVV/BIVV_CONT_EXCL_V.sql
@@ -1,6 +1,6 @@
 CREATE OR REPLACE VIEW BIVV.BIVV_CONT_EXCL_V AS 
 -- if more exceptions are needed then add them to select from dual as comma seperated values
-WITH t AS (SELECT 'MEDISUPPNMPI' AS cont_internal_id FROM dual)
+WITH t AS (SELECT 'MEDISUPPNMPI,NYMCAPSUPP' AS cont_internal_id FROM dual)
 SELECT 
    cont_internal_id AS all_excl_cont_id
    ,trim(COLUMN_VALUE) AS cont_internal_id   
